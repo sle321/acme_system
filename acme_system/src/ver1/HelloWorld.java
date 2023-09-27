ff820b335c7bc7727f4ded0a64673dd3fb402f57
Samuel Echols <samuelechols321@gmail.com>
2023-09-27 18:54:00 -0400

    working locally

--- acme_system/src/ver1/HelloWorld.java
+++ acme_system/src/ver1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		System.out.println("Sam is working locally on his branch");
 
 	}
 
